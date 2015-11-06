a97ae237fe644213695b7f6b5daf64bd4f89d2bd
AnaLucia20 <analuciarodriguez.a@gmail.com>
2015-11-06 11:37:14 -0500

    portaflio-lu

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
-$(document).ready(function() {
-  $('.chart').easyPieChart({
-    scaleColor: "#ecf0f1",
-    lineWidth: 13,
-    lineCap: 'butt',
-    barColor: '#66A366',
-    trackColor:	"#ecf0f1",
-    size: 125,
-    animate: 500
-  });
-}); 
+$(function(event) {
+	
+	$(window).scroll(function(event) { //scroll
+		
+		if($('#habilidades').is(':appeared')){
+			
+			$('.chart').easyPieChart({ //if aparece ejecuta el script
+				scaleColor: "#ecf0f1",
+				lineWidth: 15,
+				lineCap: 'butt',
+				barColor: '#1abc9c',
+				trackColor:	"#ecf0f1",
+				size: 160,
+				animate: 1800
+			  });
+		};
+	
+	});
 
-/*$(document).scroll(function () {
-var scroll = $("#habilidades").scrollTop();
 
-if 
-$(document).scroll(function() {
-  var scroll = $(this).scrollTop();
-
-  if (scroll >= 600) {
-    $('#habilidades').addClass("chart");
-  } else {
-    $('#habilidades').removeClass("chart");
-  }
- })*/
+});
