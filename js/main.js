e909a4842e71728ba9cd402b53f5a63b8d0d248d
AnaLucia20 <analuciarodriguez.a@gmail.com>
2017-01-10 09:39:33 -0500

    fixes video

--- js/main.js
+++ js/main.js
@@ -11,11 +11,38 @@ $(function(event) {
 				barColor: '#1abc9c',
 				trackColor:	"#ecf0f1",
 				size: 160,
-				animate: 1800
+				animate: 1800,
 			  });
 		};
 	
 	});
 
+	///SOLO ESTO
+
+
+	$(document).scroll(function () {
+			var scroll = $(this).scrollTop();
+
+			if (scroll >= 425) {
+
+
+					// $(".nav.navbar-nav>li>a").css("color", "#fff");
+					$(".nav_green").addClass("top_fix");
+					$(".space").addClass("pd_5");		
+				} else {
+
+					$(".nav_green").removeClass("top_fix");	
+					$(".space").removeClass("pd_80");						
+				}
+
+	});
+
+
+
+	$(document).ready(function(){
+	  $('.parallax').parallax();
+	});
 
 });
+
+
